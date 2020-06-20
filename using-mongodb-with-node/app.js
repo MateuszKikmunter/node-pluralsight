236c0bd50323d2c044f6f23d479f04b602666aa0
m.kikmunter <M.Kikmunter@gmail.com>
2020-06-20 21:31:42 +0100

    adding new item

--- using-mongodb-with-node/app.js
+++ using-mongodb-with-node/app.js
@@ -29,6 +29,19 @@ async function main () {
         const byId = await circulationRepo.getById(id);
         assert.deepEqual(byId, getData[4]);
 
+        const newItem = {
+            "Newspaper": "Los Santos Paper",
+            "Daily Circulation, 2004": 1,
+            "Daily Circulation, 2013": 2,
+            "Change in Daily Circulation, 2004-2013": 100,
+            "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
+            "Pulitzer Prize Winners and Finalists, 2004-2014": 0,
+            "Pulitzer Prize Winners and Finalists, 1990-2014": 0
+        };
+        const addedItem = await circulationRepo.add(newItem);
+        const addedItemQuery = await circulationRepo.getById(addedItem._id);
+        assert.deepEqual(addedItemQuery, newItem);
+
     } catch (error) {
 
         console.log(error);
--- using-mongodb-with-node/repos/circulationRepo.js
+++ using-mongodb-with-node/repos/circulationRepo.js
@@ -40,6 +40,22 @@ function circulationRepo() {
         });
     };
 
+    function add(item) {
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(url);
+            try {
+                await client.connect();
+                const db = client.db(dbName);
+                const addedItem = await db.collection("newspapers").insertOne(item);
+
+                resolve(addedItem.ops[0]);
+                client.close();
+            } catch (error) {
+                reject(error);
+            }
+        });
+    };
+
     function loadData(data) {
         return new Promise(async (resolve, reject) => {
             const client = new MongoClient(url);
@@ -60,6 +76,7 @@ function circulationRepo() {
     return {
         get,
         getById,
+        add,
         loadData
     };
 }
