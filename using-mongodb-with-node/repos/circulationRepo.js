dba6b93b71e58121c0c6dc6d8a4929e2ad8fb312
m.kikmunter <M.Kikmunter@gmail.com>
2020-06-20 20:40:59 +0100

    using get

--- using-mongodb-with-node/repos/circulationRepo.js
+++ using-mongodb-with-node/repos/circulationRepo.js
@@ -5,6 +5,22 @@ function circulationRepo() {
     const url = "mongodb://localhost:27017";
     const dbName = "circulation";
 
+    function get() {
+        return new Promise(async (resolve, reject) => {
+            const client = new MongoClient(url);
+            try {
+                await client.connect();
+                const db = client.db(dbName);
+
+                const items = db.collection("newspapers").find();
+                resolve(await items.toArray());
+                client.close();
+            } catch (error) {
+                reject(error);
+            }
+        });
+    };
+
     function loadData(data) {
         return new Promise(async (resolve, reject) => {
             const client = new MongoClient(url);
@@ -14,16 +30,18 @@ function circulationRepo() {
 
                 const results = await db.collection("newspapers").insertMany(data);
                 resolve(results);
+                client.close();
 
             } catch (error) {
                 reject(error);
             }
         });
-    }
+    };
 
     return {
+        get,
         loadData
-    }
+    };
 }
 
 module.exports = circulationRepo();
