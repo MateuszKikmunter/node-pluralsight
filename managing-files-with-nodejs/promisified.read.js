c699f6d4edb130882032d6e3593c370b94d6c5f3
m.kikmunter <M.Kikmunter@gmail.com>
2020-07-04 19:59:58 +0100

    using promises and await

---
+++ managing-files-with-nodejs/promisified.read.js
@@ -0,0 +1,16 @@
+const { convertCsv } = require("./exercise-files/csv.parse");
+const fs = require("fs");
+const { promisify } = require("util");
+
+const readFile = promisify(fs.readFile);
+
+readFile("./exercise-files/data/pulitzer-circulation-data.csv", "utf-8")
+    .then(data => console.table(convertCsv(data)))
+    .catch(error => console.log(error));
+
+const read = async () => {
+    const data = await readFile("./exercise-files/data/pulitzer-circulation-data.csv", "utf-8");
+    console.table(convertCsv(data));
+};
+
+read();
