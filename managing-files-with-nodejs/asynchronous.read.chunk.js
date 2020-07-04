0b4ab2f097b15129190551b5521201b1f1b1899e
m.kikmunter <M.Kikmunter@gmail.com>
2020-07-04 20:20:54 +0100

    read chunk asyc with errors

---
+++ managing-files-with-nodejs/asynchronous.read.chunk.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+let totalSize = 0;
+
+//get size of the file
+fs.stat("./exercise-files/data/app.log", (error, { size }) => totalSize = size);
+
+fs.open("./exercise-files/data/app.log", (error, fd) => {
+    const buffer = Buffer.alloc(200);
+
+    for (let i = 0; i <= totalSize / buffer.length; i++) {
+        fs.read(fd, buffer, 0, buffer.length, i * buffer.length, (error, count, buff) => {
+            console.log(buff.toString());
+        });
+    }
+
+});
