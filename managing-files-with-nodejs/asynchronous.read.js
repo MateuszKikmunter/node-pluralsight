b15e604395d75e2af1fcbf7d5ee7fa910fc2e2dd
m.kikmunter <M.Kikmunter@gmail.com>
2020-07-04 19:48:27 +0100

    handle error

--- managing-files-with-nodejs/asynchronous.read.js
+++ managing-files-with-nodejs/asynchronous.read.js
@@ -2,6 +2,10 @@ const { convertCsv } = require("./exercise-files/csv.parse");
 const { readFile } = require("fs");
 
 readFile("./exercise-files/data/pulitzer-circulation-data.csv", "utf-8", (err, data) => {
+    if(err) {
+        console.log(`There was a problem with the file ${ err }`);
+        return;
+    }
     const values = convertCsv(data);
     console.table(values);
 });
