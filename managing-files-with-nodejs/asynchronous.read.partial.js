6b98c1c2413d049d7b4337884024c7b0c025397a
m.kikmunter <M.Kikmunter@gmail.com>
2020-07-04 20:11:00 +0100

    reading part of a file

---
+++ managing-files-with-nodejs/asynchronous.read.partial.js
@@ -0,0 +1,20 @@
+const { convertCsv } = require("./exercise-files/csv.parse");
+const { open, read } = require("fs");
+
+//fd === file descriptior
+open("./exercise-files/data/pulitzer-circulation-data.csv", (err, fd) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+
+    const buffer = Buffer.alloc(200);
+    read(fd, buffer, 0, buffer.length, 0, (err, count, buff) => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+
+        console.table(convertCsv(buff.toString()));
+    });
+});
