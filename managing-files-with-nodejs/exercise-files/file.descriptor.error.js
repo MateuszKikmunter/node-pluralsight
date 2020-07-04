ee6bc980980f0e0777471663345c477ad4b1008c
m.kikmunter <M.Kikmunter@gmail.com>
2020-07-04 20:33:21 +0100

    remember to close file when using file descriptor

--- managing-files-with-nodejs/exercise-files/file.descriptor.error.js
+++ managing-files-with-nodejs/exercise-files/file.descriptor.error.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 
+// !!WHEN USING FILE DESCRIPTOR WE HAVE TO CLOSE THE FILE MANUALLY !!
 console.log('Opening files...');
 for (let i = 0; i < 50000; i++) {
   const fd = fs.openSync("./data/app.log");
+  console.log(fd);
+  fs.closeSync(fd);
 }
