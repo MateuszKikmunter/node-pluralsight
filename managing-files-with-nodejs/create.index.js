4ca8226ba6f5eb7e3497721e3f185b0189583714
m.kikmunter <M.Kikmunter@gmail.com>
2020-07-06 14:03:54 +0100

    watching for changes

--- managing-files-with-nodejs/create.index.js
+++ managing-files-with-nodejs/create.index.js
@@ -1,14 +1,17 @@
-const { closeSync, openSync, writeSync, readdirSync } = require("fs");
+const { closeSync, openSync, writeSync, readdirSync, watch } = require("fs");
 const camelcase = require("camelcase");
 
-const indexFd = openSync("./index.js", "w");
-const files = readdirSync("./read");
-
-files.map(file => {
-    const name = file.replace("js", "");
-    console.log(`Addind a file: ${file}`);
-
-    writeSync(indexFd, `module.exports.${camelcase(name)} = require("./read/${name}").read;\n`);
+//watch for changes in the read directory
+watch("./read", () => {
+    const indexFd = openSync("./index.js", "w");
+    const files = readdirSync("./read");
+    
+    files.map(file => {
+        const name = file.replace("js", "");
+        console.log(`Addind a file: ${file}`);
+    
+        writeSync(indexFd, `module.exports.${camelcase(name)} = require("./read/${name}").read;\n`);
+    });
+    
+    closeSync(indexFd);
 });
-
-closeSync(indexFd);
