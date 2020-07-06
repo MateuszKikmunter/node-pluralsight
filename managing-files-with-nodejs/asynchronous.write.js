747a2be856b0b00e202b72d9e8e634232bf102f1
m.kikmunter <M.Kikmunter@gmail.com>
2020-07-06 13:37:54 +0100

    using differenyt writing modes

--- managing-files-with-nodejs/asynchronous.write.js
+++ managing-files-with-nodejs/asynchronous.write.js
@@ -1,5 +1,13 @@
-const { writeFile } = require("fs");
+const { constants, writeFile } = require("fs");
 
-writeFile("./exercise-files/data/app.log", '163.3.217.18 - - [21/09/2019:10:07:21 -0500] "GET /write-file-test" 405 21512', (err) => {
+writeFile("./exercise-files/data/newapp.log", '163.3.217.18 - - [21/09/2019:10:07:21 -0500] "GET /write-file-test" 405 21512', {
+    flag: "wx"
+},(err) => {
+    err ? console.log(err) : console.log("File saved!");
+});
+
+writeFile("./exercise-files/data/newapp.log", '163.3.217.18 - - [21/09/2019:10:07:21 -0500] "GET /write-file-test" 405 21512', {
+    mode: constants.S_IWUSR | constants.S_IRUSR
+},(err) => {
     err ? console.log(err) : console.log("File saved!");
 });
