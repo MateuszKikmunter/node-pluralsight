07739ed7c813e140be939b37ffe38844b0c5f3b1
m.kikmunter <M.Kikmunter@gmail.com>
2020-07-06 13:59:45 +0100

    creating index.js file with all reading functions

--- managing-files-with-nodejs/create.index.js
+++ managing-files-with-nodejs/create.index.js
@@ -1,2 +1,14 @@
+const { closeSync, openSync, writeSync, readdirSync } = require("fs");
+const camelcase = require("camelcase");
 
+const indexFd = openSync("./index.js", "w");
+const files = readdirSync("./read");
 
+files.map(file => {
+    const name = file.replace("js", "");
+    console.log(`Addind a file: ${file}`);
+
+    writeSync(indexFd, `module.exports.${camelcase(name)} = require("./read/${name}").read;\n`);
+});
+
+closeSync(indexFd);
